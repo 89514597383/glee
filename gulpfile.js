203076d92264e3ca0d64a6a217e63efebeeb14f3
89514597382 <kustubaev2016@yandex.ru>
2021-05-18 20:27:01 +0300

    added slider

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,27 @@
-  const { src, dest, watch, parallel, series} = require('gulp');
+const { src, dest, watch, parallel, series } = require('gulp');
 
-  const scss                 = require('gulp-sass');
-  const concat               = require('gulp-concat');
-  const autoprefixer         = require('gulp-autoprefixer');
-  const uglify               = require('gulp-uglify');
-  const imagemin             = require('gulp-imagemin');
-  const del                  = require('del');
-  const browserSync          = require('browser-sync').create();
-
-
-  function browsersync() {
-    browserSync.init({
-      server: {
-        baseDir: 'app/'
-      },
-      notofy: false
-    })
-  }
+const scss = require('gulp-sass');
+const concat = require('gulp-concat');
+const autoprefixer = require('gulp-autoprefixer');
+const uglify = require('gulp-uglify');
+const imagemin = require('gulp-imagemin');
+const del = require('del');
+const browserSync = require('browser-sync').create();
 
 
-  function styles(){
-    return src('app/scss/style.scss')
-    .pipe(scss({outputStyle: 'compressed'}))
+function browsersync() {
+  browserSync.init({
+    server: {
+      baseDir: 'app/'
+    },
+    notofy: false
+  })
+}
+
+
+function styles() {
+  return src('app/scss/style.scss')
+    .pipe(scss({ outputStyle: 'compressed' }))
     .pipe(concat('style.min.css'))
     .pipe(autoprefixer({
       overrideBrowserslist: ['last 10 versions'],
@@ -29,23 +29,24 @@
     }))
     .pipe(dest('app/css'))
     .pipe(browserSync.stream())
-  }
+}
 
 
-  function scripts() {
-    return src([
-      'node_modules/jquery/dist/jquery.js',
-      'app/js/main.js'
-    ])
+function scripts() {
+  return src([
+    'node_modules/jquery/dist/jquery.js',
+    'node_modules/slick-carousel/slick/slick.js',
+    'app/js/main.js',
+  ])
     .pipe(concat('main.min.js'))
     .pipe(uglify())
     .pipe(dest('app/js'))
     .pipe(browserSync.stream())
-  }
+}
 
 
-  function images() {
-    return src('app/images/**/*.*')
+function images() {
+  return src('app/images/**/*.*')
     .pipe(imagemin([
       imagemin.gifsicle({ interlaced: true }),
       imagemin.mozjpeg({ quality: 75, progressive: true }),
@@ -58,43 +59,43 @@
       })
     ]))
     .pipe(dest('dist/images'))
-  }
+}
 
 
-  function build() {
-    return src([
-      'app/**/*.html',
-      'app/css/style.min.css',
-      'app/js/main.min.js',
-    ], {base: 'app'})
+function build() {
+  return src([
+    'app/**/*.html',
+    'app/css/style.min.css',
+    'app/js/main.min.js',
+  ], { base: 'app' })
     .pipe(dest('dist'))
-  }
+}
 
 
-  function cleanDist() {
-    return del('dist')
-  }
+function cleanDist() {
+  return del('dist')
+}
 
 
-  function watching() {
-    watch(['app/scss/**/*.scss'], styles);
-    watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts);
-    watch(['app/**/*.html']).on('change', browserSync.reload);
-  }
+function watching() {
+  watch(['app/scss/**/*.scss'], styles);
+  watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts);
+  watch(['app/**/*.html']).on('change', browserSync.reload);
+}
 
 
-  exports.styles      = styles;
-  exports.scripts     = scripts;
-  exports.browsersync = browsersync;
-  exports.watching    = watching;
-  exports.cleanDist   = cleanDist;
-  exports.images      = images;
+exports.styles = styles;
+exports.scripts = scripts;
+exports.browsersync = browsersync;
+exports.watching = watching;
+exports.cleanDist = cleanDist;
+exports.images = images;
 
-  // Используем во время работы 'gulp default' или 'gulp'
-  exports.default     = parallel(styles, scripts, browsersync, watching);
+// Используем во время работы 'gulp default' или 'gulp'
+exports.default = parallel(styles, scripts, browsersync, watching);
 
-  // Используем после работы 'gulp build'
-  exports.build       = series(cleanDist, images, build);
+// Используем после работы 'gulp build'
+exports.build = series(cleanDist, images, build);
 
 
 
