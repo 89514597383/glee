b66692d42b7bf985d5c0d316a44795f4c34f6924
89514597382 <kustubaev2016@yandex.ru>
2021-05-27 21:15:53 +0300

    added shop

--- app/js/main.js
+++ app/js/main.js
@@ -1,11 +1,44 @@
 $(function () {
 
+  $('.shop-content__filter-btn').on('click', function () {
+    $('.shop-content__filter-btn').removeClass('shop-content__filter-btn--active');
+    $(this).addClass('shop-content__filter-btn--active');
+  });
+
+  $('.button-list').on('click', function () {
+    $('.product__item').addClass('product__item--list');
+  });
+
+  $('.button-grid').on('click', function () {
+    $('.product__item').removeClass('product__item--list');
+  });
+
+  $('.filter-price__input').ionRangeSlider({
+    type: "double",
+    prefix: "$",
+    onStart: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+    onChange: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+  });
+
   $('.top-slider__inner').slick({
     dots: true,
     arrows: false,
     fade: true,
     autoplay: true,
     autoplaySpeed: 2000,
+  });
+
+  $(".star").rateYo({
+    starWidth: "12px",
+    normalFill: "#d6d6d6",
+    ratedFill: "#ffcc00",
+    readOnly: true,
   });
 
   var filter__1 = document.querySelector('[data-ref="filter-1"]');
--- gulpfile.js
+++ gulpfile.js
@@ -25,7 +25,7 @@ function styles() {
     .pipe(concat('style.min.css'))
     .pipe(autoprefixer({
       overrideBrowserslist: ['last 10 versions'],
-      grid: true
+      grid: true,
     }))
     .pipe(dest('app/css'))
     .pipe(browserSync.stream())
@@ -36,8 +36,11 @@ function scripts() {
   return src([
     'node_modules/jquery/dist/jquery.js',
     'node_modules/slick-carousel/slick/slick.js',
-    'node_modules/mixitup/dist/mixitup.js',
     'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
+    'node_modules/mixitup/dist/mixitup.js',
+    'node_modules/rateyo/src/jquery.rateyo.js',
+    'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
+    // 'node_modules/jquery-form-styler/dist/jquery.formstyler.js',
     'app/js/main.js',
   ])
     .pipe(concat('main.min.js'))
